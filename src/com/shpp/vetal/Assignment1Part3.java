46a55343e62eec4d477b44613ec842600038b040
Vitaliy Lyashchenko <69591510+Joroboro253@users.noreply.github.com>
2023-08-29 15:21:11 +0300

    Assignment 3 done

--- src/com/shpp/vetal/Assignment1Part3.java
+++ src/com/shpp/vetal/Assignment1Part3.java
@@ -3,4 +3,42 @@ package com.shpp.vetal;
 import com.shpp.karel.KarelTheRobot;
 
 public class Assignment1Part3 extends KarelTheRobot {
+    int amountOfLines = 0;
+    public void run() throws Exception {
+        amountOfLines = countingLines();
+        turnAround();
+        goToMidpoint(findMidpoint(amountOfLines));
+        putBeeper();
+    }
+
+//    private void turnRight() throws Exception {
+//        for (int i = 0; i < 3; i++) {
+//            turnLeft();
+//        }
+//    }
+
+    private int countingLines() throws Exception {
+        int lines = 0;
+        while (!frontIsBlocked()) {
+            lines++;
+            move();
+        }
+        return lines;
+    }
+
+    private int findMidpoint(int amountOfLines){
+        return amountOfLines / 2;
+    }
+
+    private void goToMidpoint(int midpoint) throws Exception {
+        for (int i = 0; i < midpoint; i++) {
+            move();
+        }
+    }
+
+    private void turnAround() throws Exception {
+        for (int i = 0; i < 2; i++) {
+            turnLeft();
+        }
+    }
 }
--- src/com/shpp/vetal/Assignment1Part4.java
+++ src/com/shpp/vetal/Assignment1Part4.java
@@ -3,4 +3,5 @@ package com.shpp.vetal;
 import com.shpp.karel.KarelTheRobot;
 
 public class Assignment1Part4 extends KarelTheRobot {
+
 }
